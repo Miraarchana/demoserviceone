fb12cb645e5b9e96115216d6dfb2d2cce4b26fb0
Archana Venkataraman <miraarchana@gmail.com>
2019-09-06 23:30:02 -0700

    demoserviceone initial service

--- demoserviceone/src/main/java/com/example/DemoserviceoneApplication.java
+++ demoserviceone/src/main/java/com/example/DemoserviceoneApplication.java
@@ -2,15 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DemoserviceoneApplication {
-//	@Bean
-//	public RestTemplate getRestTemplate() {//this is executed only once
-//		return new RestTemplate();
-//	}//alternate to REstTemplateBuilder
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {//this is executed only once
+		return new RestTemplate();
+	}//alternate to REstTemplateBuilder
 	public static void main(String[] args) {
 		SpringApplication.run(DemoserviceoneApplication.class, args);
 	}
--- demoserviceone/src/main/java/com/example/DemoserviceoneController.java
+++ demoserviceone/src/main/java/com/example/DemoserviceoneController.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,17 +10,19 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class DemoserviceoneController {
+	@Autowired
     private RestTemplate restTemplate;
 	
-	@Autowired
-	public DemoserviceoneController(RestTemplateBuilder builder) {//alternate is create a bean of rest template and autowire it when needed.
-	    this.restTemplate = builder.build();
-	}
+//	
+//	@LoadBalanced
+//	public DemoserviceoneController(RestTemplateBuilder builder) {//alternate is create a bean of rest template and autowire it when needed.
+//	    this.restTemplate = builder.build();
+//	}
 	
 	@RequestMapping("/{name}")
 	public String hello(@PathVariable("name") String name) {
 		System.out.println("inside controller1");
-		return restTemplate.getForObject("http://localhost:8082/"+name,String.class);
+		return restTemplate.getForObject("http://demoservicetwo/"+name,String.class);
 //		return "Hello "+ name;
 	}
 
